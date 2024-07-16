01ebccbb87e804b6cfcbfc05d796ac9ebce2db2d
Louis Ledoux <louis.ledoux@epita.fr>
2024-07-16 18:52:40 +0200

    feat: username

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -2,5 +2,6 @@ class ConversationsController < ApplicationController
   before_action :authenticate_user!
   
   def index
+    @users = User.where("username LIKE ?", "%#{params[:search]}%")
   end
 end
