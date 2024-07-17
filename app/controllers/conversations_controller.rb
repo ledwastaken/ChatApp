0f6d11acbb5deb19621b152d89ea672958bd61e8
Louis Ledoux <louis.ledoux@epita.fr>
2024-07-17 08:08:21 +0200

    feat: application.css

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -12,7 +12,7 @@ class ConversationsController < ApplicationController
     else
       @conversation = Conversation.create!(conversation_params)
     end
-    redirect_to conversation_messages_path(@conversation)
+    redirect_to conversation_messages_path(@conversation, locale: I18n.locale)
   end
 
   def show
